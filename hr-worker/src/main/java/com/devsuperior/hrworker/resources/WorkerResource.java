c37f9b1153470270f217c5eb325793ff92563b9c
WendellLima <43830716+WendellLima@users.noreply.github.com>
2021-10-10 16:37:33 -0300

    db config server

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +22,6 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 public class WorkerResource {
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -34,7 +31,7 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = " + testConfig);
+
 		return ResponseEntity.noContent().build();
 	}	
 	
@@ -46,13 +43,13 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findbyId(@PathVariable Long id){
-		
+		/*
 		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+		*/
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
